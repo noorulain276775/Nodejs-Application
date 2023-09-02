364265fe643934bf6d42688aefd60c9853e44d8a
Bilal Ahmed <bilalahmedpak.03@gmail.com>
2023-09-02 23:13:20 +0400

    file added in gitignore

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,9 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const products = require('./routes/product');
+app.use(express.json());
 
+
+
+app.use('/api/', products)
 module.exports = app
