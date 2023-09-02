c37197c247d79bf602bc53cb2fafa3f40ad89722
Bilal Ahmed <bilalahmedpak.03@gmail.com>
2023-09-02 22:39:53 +0400

    started application backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("This is the entry point fo the application")
+const app = require("./app");
+const dotenv = require("dotenv");
+
+// setting up config file
+dotenv.config({ path: '../backend/config/config.env' });
+const port = process.env.PORT;
+const nodeEnv = process.env.NODE_ENV;
+app.listen(process.env.PORT, () => {
+  console.log(
+    `server started at port number ${port} in ${nodeEnv} mode`
+  );
+});
