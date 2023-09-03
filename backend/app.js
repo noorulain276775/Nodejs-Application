7a8a7458b9a9ee94aced2a07430aaeb2816c29dc
Bilal Ahmed <bilalahmedpak.03@gmail.com>
2023-09-03 14:25:57 +0400

    database mongodb connected and product schema added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
-const products = require('./routes/product');
+const products = require("./routes/product");
 app.use(express.json());
-
-
-
-app.use('/api/', products)
-module.exports = app
+app.use("/api/", products);
+module.exports = app;
